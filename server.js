ae5dd83252f375d1a06c5ea40cf60dd67cd39077
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-08-17 18:26:54 -0400

    fix bugs

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const apiRoute = require('./routes/api');
-const htmlRoute = require('./routes/html');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 
 const PORT = process.env.PORT || 3001;
@@ -14,8 +14,8 @@ app.use(express.urlencoded({
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use('/api', apiRoute);
-app.use('/', htmlRoute);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {

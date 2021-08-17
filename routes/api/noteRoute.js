64e43a47f181ff79bde80b971d1c2921a1aea5c5
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-08-17 17:44:17 -0400

    added api routes for get, post, and delete request.

--- routes/api/noteRoute.js
+++ routes/api/noteRoute.js
@@ -1,3 +1,24 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db');
-const { createNewNote}
+const { createNewNote, deleteNote } = require('../../lib/noteFunction');
+
+
+router.get('/notes', (req, res) => {
+    let savedNote = notes;
+    res.json(savedNote);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    let postNote = createNewNote(req.body, notes);
+    res.json(postNote);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
+});
+
+
+module.exports = router;
+
